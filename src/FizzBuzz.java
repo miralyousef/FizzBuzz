517477a08fff8f8ad2a95eb7268f9a3f96d61421
miral <miral.yousef@mail.utoronto.ca>
2024-09-13 05:30:52 -0400

    called fizz buzz from another class

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,18 +2,13 @@
  * Solve the FizzBuzz challenge.
  */
 class FizzBuzz {
-
     public static void main(String[] args) {
-
         int i = 1;
         while (i < 100) {
-
             i = doFizzBuzz(i);
-
         }
     }
-
-    private static int doFizzBuzz(int i) {
+    public static int doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+class otherClass{
+    public static void main(String[] args) {
+        FizzBuzz obj = new FizzBuzz();
+        for(int i = 1; i <= 5; i++){
+            obj.doFizzBuzz(i);
+        }
+    }
+}
